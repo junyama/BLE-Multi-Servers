fac66ca89cce6facfeb2b316fd4a037bd213c672
*junyama* <yamazaki.junichi@gmail.com>
2025-06-20 12:13:36 +0900

    notification at a separate file

--- src/main.cpp
+++ src/main.cpp
@@ -13,15 +13,15 @@
 #include <WiFiMulti.h>
 #include <PubSubClient.h>
 #include <StreamUtils.h>
-
 #include <NimBLEDevice.h>
-
 #include <M5Core2.h>
 
 #include "PowerSaving2.hpp"
 #include "MyLcd2.hpp"
 #include "MyBLE.cpp"
 #include "MyScanCallBacks.cpp"
+#include "MyNotification.hpp"
+
 #include "MyClientCallBacks.cpp"
 
 #include "MyLog.cpp"
@@ -57,13 +57,6 @@ void saveConfig();
 void wifiScann();
 int wifiConnect();
 void setupDateTime();
-
-/** Notification / Indication receiving handler callback */
-void notifyCB(NimBLERemoteCharacteristic *pRemoteCharacteristic, uint8_t *pData, size_t length, bool isNotify);
-int getIndexOfMyBleArr(NimBLEClient *client);
-
-/** Handles the provisioning of clients and connects / interfaces with the server */
-bool connectToServer();
 
 void detectButton(int numberOfPages);
 void printBatteryInfo(int bleIndex, int numberOfAdvDevices, MyBLE myBle);
